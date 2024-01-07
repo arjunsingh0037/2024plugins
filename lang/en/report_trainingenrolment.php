297ffc3ba83c5989aa5564966deab5660171dc7d
Moodle Arjun <arjunsingh0037@gmail.com>
2024-01-08 01:13:59 +0530

    Add files via upload

---
+++ lang/en/report_trainingenrolment.php
@@ -0,0 +1,95 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'report_trainingenrolment'.
+ *
+ * @package   report_trainingenrolment
+ * @category  Report Plugins
+ * @copyright 2018 Sandeep Gill {support@lingellearning.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['change']                   = 'Change';
+$string['changedby']                = 'Changed by';
+$string['trainingenrolment']        = 'Training Enrolment Report';
+$string['pluginname']               = 'COVID reporting dashboard';
+$string['timemodified']             = 'Time modified';
+$string['send_mailtousers']         = 'Send mail';
+$string['bulk_mail']                = 'Send Mail: Bulk Users';
+$string['training_report']          = 'Training Enrolment';
+$string['view_report']              = 'View Report';
+$string['enablepdf']                = 'Enables PDF attachment';
+$string['enablepdf_help']           = 'Sends the pdf file as attachments with the mail.';
+$string['subject']                  = 'Mail subject';
+$string['subject_help']             = 'This adds a custom subject to the mail.';
+$string['message']                  = 'Mail body';
+$string['message_help']             = 'This adds a custom message to be sent as mail body.';
+$string['emailids']                 = 'Email Ids';
+$string['emailids_help']            = 'Email ids (comma seperated values) of users to which mail has to be sent.';
+$string['task:process_mail']        = 'Process training enrolment mails';
+$string['outgoingmail']             = 'Covid Report : ';
+$string['filename']                 = 'CovidReport';
+$string['datefilter']               = 'Filter by date';
+$string['submitbtn']                = 'Submit';
+$string['datelabel']                = 'Select date';
+$string['reportviewed']             = 'Report viewed';
+$string['reportdownloaded']         = 'Report downloaded';
+$string['mailsent']                 = 'Mail sent';
+$string['task:process_data']        = 'Process reporting data';
+$string['module_1']                 = 'Module 1';
+$string['module_1_desc']            = 'Enter comma separated list of the module 1 course module ids';
+$string['module_2']                 = 'Module 2';
+$string['module_2_desc']            = 'Enter comma separated list of the module 2 course module ids';
+$string['module_3']                 = 'Module 3';
+$string['module_3_desc']            = 'Enter comma separated list of the module 3 course module ids';
+$string['module_4']                 = 'Module 4';
+$string['module_4_desc']            = 'Enter comma separated list of the module 4 course module ids';
+$string['module_5']                 = 'Module 5';
+$string['module_5_desc']            = 'Enter comma separated list of the module 5 course module ids';
+$string['module_6']                 = 'Module 6';
+$string['module_6_desc']            = 'Enter comma separated list of the module 6 course module ids';
+$string['pfizer_modules']           = 'Pfizer modules';
+$string['pfizer_modules_desc']      = 'Enter comma separated list of the Pfizer course module ids';
+$string['astrazeneca_modules']      = 'Astrazeneca modules';
+$string['astrazeneca_modules_desc'] = 'Enter comma separated list of the Astrazeneca course module ids';
+$string['moderna_modules']          = 'Moderna modules';
+$string['moderna_modules_desc']     = 'Enter comma separated list of the Moderna course module ids';
+$string['pfizer2_modules']           = 'Pfizer paediatric modules';
+$string['pfizer2_modules_desc']      = 'Enter comma separated list of the Pfizer Paediatric course module ids';
+$string['dashboard_sync_time']      = 'Sync time delay (seconds)';
+$string['dashboard_sync_time_desc'] = 'Sync records that have been updated in the last {seconds} seconds ';
+$string['dbhost']                   = 'Remote DB host';
+$string['dbhost_desc']              = '';
+$string['dbname']                   = 'Remote DB name';
+$string['dbname_desc']              = '';
+$string['dbuser']                   = 'Remote DB user';
+$string['dbuser_desc']              = '';
+$string['dbpass']                   = 'Remote DB password';
+$string['dbpass_desc']              = '';
+$string['participantdetails']       = 'Participant details report';
+$string['module']                   = 'Module';
+$string['ahpra_number']             = 'Registration number';
+$string['home_state']               = 'State';
+$string['completiondate']           = 'Completion Date';
+$string['cachedef_dashboard']       = 'Cache definition for the dashboard';
+$string['postcodecompletions']      = 'Completions by postcode';
+$string['ignore_users']             = 'Ignore users from reports';
+$string['ignore_users_desc']        = 'Comma separated list of userids that you want removed from report calculations.';
+
+// Mustache Template Strings
+$string['exportbutton']       = 'Export to PDF';
+$string['enrolment_mainhead'] = 'COVID-19 VACCINATION TRAINING PROGRAM:ENROLMENTS';
